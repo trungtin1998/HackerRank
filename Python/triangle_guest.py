95af3c7fe5deda84d361edabb467f1d9ca160394
trungtin1998 <48832546+trungtin1998@users.noreply.github.com>
2021-05-23 15:35:32 +0700

    Update triangle_guest.py

--- Python/triangle_guest.py
+++ Python/triangle_guest.py
@@ -1,4 +1,5 @@
 # Link to chall: https://www.hackerrank.com/challenges/python-quest-1/problem
-# Can't use string manipulation or more than 2 loops, therefore: `print(i*"{}".format(i))` not accepted
+# You are given a positive integer . Print a numerical triangle of height  like the one below:
+# You can't use string manipulation or more than 2 loops, therefore: `print(i*"{}".format(i))` not accepted
 for i in range(1,int(input())): #More than 2 lines will result in 0 score. Do not leave a blank line also
     print(i * pow(10,i)//9)
