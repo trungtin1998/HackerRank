10f354fe4fa99e9a59079c12b11608744f7da774
trungtin1998 <48832546+trungtin1998@users.noreply.github.com>
2021-05-26 13:11:23 +0700

    Update maximize_it.py

--- Python/maximize_it.py
+++ Python/maximize_it.py
@@ -1,4 +1,5 @@
-# 
+# Link to problem: https://www.hackerrank.com/challenges/maximize-it/problem
+# Enter your code here. Read input from STDIN. Print output to STDOUT
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 from itertools import combinations, product
 k, m = list(map(int, input().split()))
@@ -6,9 +7,7 @@ l = []
 for _ in range(k):
     l.append((list(map(int, input().split()))[1:]))
 
-mymax = 0
+res = set()
 for x in list(product(*l)):
-    tmp = (sum(i**2 for i in x)) % m
-    if tmp >= mymax:
-        mymax = tmp
-print(mymax)
+    res.add((sum(i**2 for i in x)) % m)
+print(max(res))
